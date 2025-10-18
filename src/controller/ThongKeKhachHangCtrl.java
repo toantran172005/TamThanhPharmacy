ad21e76efe34dc95d7bdf817cbd7849c63eb0163
toantran172005 <toantran172005@gmail.com>
2025-10-18 20:19:01 +0700

    kh

--- src/controller/ThongKeKhachHangCtrl.java
+++ src/controller/ThongKeKhachHangCtrl.java
@@ -1,13 +1,192 @@
 package controller;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import dao.KhachHangDAO;
+import entity.KhachHang;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.ScrollEvent;
 
 public class ThongKeKhachHangCtrl {
-	
-	
+
+	public ToolCtrl tool = new ToolCtrl();
+	public KhachHangDAO khDAO = new KhachHangDAO();
+	Map<String, Double> tongTienMap = new HashMap<>();
+	Map<String, Integer> tongDonMap = new HashMap<>();
+
+	@FXML
+	public ScrollPane scrTKKH;
+	@FXML
+	public TableView<KhachHang> tblThongKeKH;
+	@FXML
+	public TableColumn<KhachHang, Integer> colSTT;
+	@FXML
+	public TableColumn<KhachHang, String> colMaKH;
+	@FXML
+	public TableColumn<KhachHang, String> colTenKH;
+	@FXML
+	public TableColumn<KhachHang, String> colSdt;
+	@FXML
+	public TableColumn<KhachHang, Integer> colSoLanMua;
+	@FXML
+	public TableColumn<KhachHang, String> colTongChiTieu;
+	@FXML
+	public ComboBox<String> cmbThongKeTop;
+	@FXML
+	public Button btnThongKe, btnXuatExcel, btnInBaoCao, btnLamMoi;
+	@FXML
+	public Label lblTongKH, lblTongSLM, lblTongCT, lblChiTieuTB;
+	@FXML
+	public DatePicker dpNgayBD, dpNgayKT;
+	@FXML
+	public BarChart<String, Number> barChartTopKH;
+
+	@FXML
+	public CategoryAxis xAxis;
+
+	@FXML
+	public NumberAxis yAxis;
+
+	public ObservableList<KhachHang> listKH = FXCollections.observableArrayList();
+
 	public void initialize() {
+		setScrCuonNhanh();
+		dinhDangNgayDatePicker();
+		setItemChoComboBox();
+		setHoatDong();
+	}
+
+	public void setDataChoTable() {
+		if (listKH == null || listKH.isEmpty()) {
+			tblThongKeKH.getItems().clear();
+			return;
+		}
+
+		colSTT.setCellValueFactory(cellData -> {
+			return new SimpleIntegerProperty(tblThongKeKH.getItems().indexOf(cellData.getValue()) + 1).asObject();
+		});
+
+		colMaKH.setCellValueFactory(new PropertyValueFactory<>("maKH"));
+		colTenKH.setCellValueFactory(new PropertyValueFactory<>("tenKH"));
+		colSdt.setCellValueFactory(cellData -> {
+			return new SimpleStringProperty(tool.chuyenSoDienThoai(cellData.getValue().getSdt()));
+		});
+		colTongChiTieu.setCellValueFactory(cellData -> new SimpleStringProperty(
+				tool.dinhDangVND(tongTienMap.getOrDefault(cellData.getValue().getMaKH(), 0.0))));
+		colSoLanMua.setCellValueFactory(
+				cellData -> new SimpleIntegerProperty(tongDonMap.getOrDefault(cellData.getValue().getMaKH(), 0))
+						.asObject());
+
+		tblThongKeKH.setItems(listKH);
+	}
+
+	public void setScrCuonNhanh() {
+		scrTKKH.addEventFilter(ScrollEvent.SCROLL, event -> {
+			double deltaY = event.getDeltaY() * 5;
+			scrTKKH.setVvalue(scrTKKH.getVvalue() - deltaY / scrTKKH.getContent().getBoundsInLocal().getHeight());
+			event.consume();
+		});
+
+		tblThongKeKH.setOnScroll(e -> {
+			double deltaY = e.getDeltaY() * 3;
+			tblThongKeKH.scrollTo((int) (tblThongKeKH.getFocusModel().getFocusedIndex() - deltaY / 40));
+		});
 
 	}
+
+	public void dinhDangNgayDatePicker() {
+		tool.dinhDangDatePicker(dpNgayBD);
+		tool.dinhDangDatePicker(dpNgayKT);
+	}
+
+	public void setHoatDong() {
+		btnThongKe.setOnAction(event -> thongKeKhachHang());
+	}
+
+	public void thongKeKhachHang() {
+		LocalDate ngayBD = dpNgayBD.getValue();
+		LocalDate ngayKT = dpNgayKT.getValue();
+
+		if (ngayBD == null) {
+			tool.hienThiThongBao("Ngày bắt đầu rỗng", "Vui lòng chọn ngày bắt đầu!", false);
+			return;
+		} else if (ngayKT == null) {
+			tool.hienThiThongBao("Ngày kết thúc rỗng", "Vui lòng chọn ngày kết thúc!", false);
+			return;
+		} else if (ngayBD.isAfter(ngayKT)) {
+			tool.hienThiThongBao("Lỗi ngày", "Ngày bắt đầu phải trước ngày kết thúc!", false);
+			return;
+		}
+
+		listKH = khDAO.layListKHThongKe(ngayBD, ngayKT);
+		for (KhachHang kh : listKH) {
+			double tongTien = khDAO.layTongTien(kh.getMaKH());
+			int tongDon = khDAO.layTongDonHang(kh.getMaKH());
+			tongTienMap.put(kh.getMaKH(), tongTien);
+			tongDonMap.put(kh.getMaKH(), tongDon);
+		}
+		setDataChoLabel();
+		setDataChoBarChart();
+		setDataChoTable();
+	}
+
+	public void setDataChoBarChart() {
+		barChartTopKH.getData().clear();
+
+		List<KhachHang> top5 = listKH.stream()
+				.sorted((kh1, kh2) -> Double.compare(kh2.getTongTien(), kh1.getTongTien())).limit(5).toList();
+
+		XYChart.Series<String, Number> series = new XYChart.Series<>();
+		series.setName("Top 5 khách hàng chi tiêu cao nhất");
+
+		for (KhachHang kh : top5) {
+			series.getData().add(new XYChart.Data<>(kh.getTenKH(), kh.getTongTien()));
+		}
+
+		barChartTopKH.getData().add(series);
+
+		xAxis.setLabel("Tên khách hàng");
+		yAxis.setLabel("Tổng chi tiêu (VND)");
+		barChartTopKH.setLegendVisible(false);
+	}
+
+	public void setDataChoLabel() {
+		lblTongKH.setText(String.valueOf(listKH.size()));
+		int tongSLM = 0;
+		for (KhachHang kh : listKH) {
+			tongSLM += kh.getTongDonHang();
+		}
+		lblTongSLM.setText(String.valueOf(tongSLM));
+		double tongCT = 0;
+		for (KhachHang kh : listKH) {
+			tongCT += kh.getTongTien();
+		}
+		lblTongCT.setText(tool.dinhDangVND(tongCT));
+		lblChiTieuTB.setText(tool.dinhDangVND(tongCT / listKH.size()));
+	}
+
+	public void setItemChoComboBox() {
+		cmbThongKeTop.getItems().addAll("1", "5", "10", "20");
+	}
+
 }
--- src/controller/ToolCtrl.java
+++ src/controller/ToolCtrl.java
@@ -16,53 +16,78 @@ import database.KetNoiDatabase;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.DatePicker;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+import javafx.util.StringConverter;
 
 public class ToolCtrl {
-	// Hiển thị thông báo 
-		public void hienThiThongBao(String tieuDe, String noiDung, Boolean trangThai) {
-			if(trangThai) {
-				try {
-					Alert thongBao = new Alert(Alert.AlertType.INFORMATION);
-					thongBao.setTitle("Thông báo");
-					thongBao.setHeaderText(tieuDe);
-					thongBao.setContentText(noiDung);
 
-					Stage stageAlert = (Stage) thongBao.getDialogPane().getScene().getWindow();
-					stageAlert.getIcons().add(new Image("/picture/trangChu/logo.jpg"));
-					ImageView icon = new ImageView(new Image(getClass().getResourceAsStream("/picture/thuoc/check.png")));
+	// định dạng date picker
+	public void dinhDangDatePicker(DatePicker datePicker) {
+	    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-					icon.setFitHeight(50);
-					icon.setFitWidth(50);
-					thongBao.setGraphic(icon);
+	    datePicker.setConverter(new StringConverter<LocalDate>() {
+	        @Override
+	        public String toString(LocalDate date) {
+	            return (date != null) ? formatter.format(date) : "";
+	        }
 
-					thongBao.showAndWait();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}else {
-				try {
-					Alert thongBao = new Alert(AlertType.INFORMATION);
-					thongBao.setTitle("Thông báo lỗi");
-					thongBao.setHeaderText(tieuDe);
-					thongBao.setContentText(noiDung);
+	        @Override
+	        public LocalDate fromString(String string) {
+	            if (string != null && !string.isEmpty()) {
+	                return LocalDate.parse(string, formatter);
+	            } else {
+	                return null;
+	            }
+	        }
+	    });
+	}
 
-					Stage stageAlert = (Stage) thongBao.getDialogPane().getScene().getWindow();
-					stageAlert.getIcons().add(new Image("/picture/trangChu/logo.jpg"));
-					ImageView icon = new ImageView(new Image(getClass().getResourceAsStream("/picture/thuoc/cross.png")));
 
-					icon.setFitHeight(50);
-					icon.setFitWidth(50);
-					thongBao.setGraphic(icon);
+	// Hiển thị thông báo
+	public void hienThiThongBao(String tieuDe, String noiDung, Boolean trangThai) {
+		if (trangThai) {
+			try {
+				Alert thongBao = new Alert(Alert.AlertType.INFORMATION);
+				thongBao.setTitle("Thông báo");
+				thongBao.setHeaderText(tieuDe);
+				thongBao.setContentText(noiDung);
 
-					thongBao.showAndWait();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+				Stage stageAlert = (Stage) thongBao.getDialogPane().getScene().getWindow();
+				stageAlert.getIcons().add(new Image("/picture/trangChu/logo.jpg"));
+				ImageView icon = new ImageView(new Image(getClass().getResourceAsStream("/picture/thuoc/check.png")));
+
+				icon.setFitHeight(50);
+				icon.setFitWidth(50);
+				thongBao.setGraphic(icon);
+
+				thongBao.showAndWait();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else {
+			try {
+				Alert thongBao = new Alert(AlertType.INFORMATION);
+				thongBao.setTitle("Thông báo lỗi");
+				thongBao.setHeaderText(tieuDe);
+				thongBao.setContentText(noiDung);
+
+				Stage stageAlert = (Stage) thongBao.getDialogPane().getScene().getWindow();
+				stageAlert.getIcons().add(new Image("/picture/trangChu/logo.jpg"));
+				ImageView icon = new ImageView(new Image(getClass().getResourceAsStream("/picture/thuoc/cross.png")));
+
+				icon.setFitHeight(50);
+				icon.setFitWidth(50);
+				thongBao.setGraphic(icon);
+
+				thongBao.showAndWait();
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
 		}
+	}
 
 	// Hiển thị thông báo xác nhận
 	public boolean hienThiXacNhan(String tieuDe, String noiDung) {
@@ -81,13 +106,14 @@ public class ToolCtrl {
 		Optional<ButtonType> ketQua = alert.showAndWait();
 		return ketQua.isPresent() && ketQua.get() == ButtonType.OK;
 	}
-	
+
 	public String dinhDangLocalDate(LocalDate date) {
-	    if (date == null) return "";
-	    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-	    return date.format(formatter);
+		if (date == null)
+			return "";
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+		return date.format(formatter);
 	}
-	
+
 	// hàm đổi từ localdate sang date
 	public java.sql.Date localDateSangSqlDate(LocalDate localDate) {
 		return (localDate != null) ? java.sql.Date.valueOf(localDate) : null;
@@ -122,101 +148,99 @@ public class ToolCtrl {
 	}
 
 	public String taoKhoaChinh(String tenBangVietTat) {
-	    // Bảng ánh xạ viết tắt -> tên bảng
-	    Map<String, String> mapBang = new HashMap<>();
-	    mapBang.put("PNT", "PhieuNhapThuoc");
-	    mapBang.put("K", "Kho");
-	    mapBang.put("TK", "TaiKhoan");
-	    mapBang.put("PKN", "Phieu_KhieuNai_HoTroKH");
-	    mapBang.put("T", "Thue");
-	    mapBang.put("KH", "KhachHang");
-	    mapBang.put("NV", "NhanVien");
-	    mapBang.put("KT", "KeThuoc");
-	    mapBang.put("NCC", "NhaCungCap");
-	    mapBang.put("TH", "Thuoc");
-	    mapBang.put("HD", "HoaDon");
-	    mapBang.put("KM", "KhuyenMai");
-	    mapBang.put("PDT", "PhieuDoiTra");
-	    mapBang.put("PDTH", "PhieuDatThuoc");
+		// Bảng ánh xạ viết tắt -> tên bảng
+		Map<String, String> mapBang = new HashMap<>();
+		mapBang.put("PNT", "PhieuNhapThuoc");
+		mapBang.put("K", "Kho");
+		mapBang.put("TK", "TaiKhoan");
+		mapBang.put("PKN", "Phieu_KhieuNai_HoTroKH");
+		mapBang.put("T", "Thue");
+		mapBang.put("KH", "KhachHang");
+		mapBang.put("NV", "NhanVien");
+		mapBang.put("KT", "KeThuoc");
+		mapBang.put("NCC", "NhaCungCap");
+		mapBang.put("TH", "Thuoc");
+		mapBang.put("HD", "HoaDon");
+		mapBang.put("KM", "KhuyenMai");
+		mapBang.put("PDT", "PhieuDoiTra");
+		mapBang.put("PDTH", "PhieuDatThuoc");
 
-	    if (!mapBang.containsKey(tenBangVietTat)) {
-	        throw new IllegalArgumentException("Không tìm thấy bảng tương ứng với mã: " + tenBangVietTat);
-	    }
-
-	    String tenBang = mapBang.get(tenBangVietTat);
-
-	    // Xử lý đặc biệt cho một số bảng
-	    String cotKhoa;
-	    switch (tenBangVietTat) {
-	        case "KT":
-	            cotKhoa = "maKe"; // KeThuoc
-	            break;
-	        case "K":
-	            cotKhoa = "maKho"; // Kho
-	            break;
-	        case "PDT":
-	            cotKhoa = "maPhieuDT"; // PhieuDoiTra
-	            break;
-	        case "PDTH":
-	            cotKhoa = "maPDT"; // PhieuDatThuoc
-	            break;
-	        case "PKN":
-	            cotKhoa = "maPhieu"; // Phieu_KhieuNai_HoTroKH
-	            break;
-	        case "T":
-	            cotKhoa = "maThue";
-	            break;
-	        default:
-	            // Mặc định: nếu tên bảng có nhiều từ viết hoa → ma + viết tắt, ngược lại ma + tên bảng
-	            if (tenBang.contains("_") || tenBang.matches(".*[A-Z].*[A-Z].*")) {
-	                cotKhoa = "ma" + tenBangVietTat;
-	            } else {
-	                cotKhoa = "ma" + tenBang;
-	            }
-	            break;
-	    }
-
-	    String prefix = "TT" + tenBangVietTat; // Ví dụ: TTTH, TTKH...
-
-	    String sql = String.format(
-	        "SELECT TOP 1 %s FROM %s WHERE %s LIKE ? " +
-	        "ORDER BY TRY_CAST(REPLACE(%s, '%s', '') AS INT) DESC",
-	        cotKhoa, tenBang, cotKhoa, cotKhoa, prefix
-	    );
-
-	    try (Connection con = KetNoiDatabase.getConnection();
-	         PreparedStatement ps = con.prepareStatement(sql)) {
-
-	        ps.setString(1, prefix + "%");
-	        ResultSet rs = ps.executeQuery();
-
-	        int so = 0;
-	        if (rs.next()) {
-	            String maMax = rs.getString(1); // VD: TTHD9
-	            String soStr = maMax.replace(prefix, ""); // -> "9"
-	            so = Integer.parseInt(soStr);
-	        }
-
-	        so++; // tăng lên 1
-	        String maMoi = prefix + so; // VD: TTHD10
-	        return maMoi;
-
-	    } catch (SQLException e) {
-	        e.printStackTrace();
-	        return null;
-	    }
-	}
-	
-	// ========== CHUYỂN CHUỖI TIỀN VỀ SỐ ==========
-		public double chuyenTienSangSo(String text) {
-		    if (text == null || text.isEmpty()) return 0;
-		    text = text.replaceAll("[^\\d.]", ""); // bỏ ký tự thừa
-		    try {
-		        return Double.parseDouble(text);
-		    } catch (NumberFormatException e) {
-		        return 0;
-		    }
+		if (!mapBang.containsKey(tenBangVietTat)) {
+			throw new IllegalArgumentException("Không tìm thấy bảng tương ứng với mã: " + tenBangVietTat);
 		}
 
+		String tenBang = mapBang.get(tenBangVietTat);
+
+		// Xử lý đặc biệt cho một số bảng
+		String cotKhoa;
+		switch (tenBangVietTat) {
+		case "KT":
+			cotKhoa = "maKe"; // KeThuoc
+			break;
+		case "K":
+			cotKhoa = "maKho"; // Kho
+			break;
+		case "PDT":
+			cotKhoa = "maPhieuDT"; // PhieuDoiTra
+			break;
+		case "PDTH":
+			cotKhoa = "maPDT"; // PhieuDatThuoc
+			break;
+		case "PKN":
+			cotKhoa = "maPhieu"; // Phieu_KhieuNai_HoTroKH
+			break;
+		case "T":
+			cotKhoa = "maThue";
+			break;
+		default:
+			// Mặc định: nếu tên bảng có nhiều từ viết hoa → ma + viết tắt, ngược lại ma +
+			// tên bảng
+			if (tenBang.contains("_") || tenBang.matches(".*[A-Z].*[A-Z].*")) {
+				cotKhoa = "ma" + tenBangVietTat;
+			} else {
+				cotKhoa = "ma" + tenBang;
+			}
+			break;
+		}
+
+		String prefix = "TT" + tenBangVietTat; // Ví dụ: TTTH, TTKH...
+
+		String sql = String.format(
+				"SELECT TOP 1 %s FROM %s WHERE %s LIKE ? " + "ORDER BY TRY_CAST(REPLACE(%s, '%s', '') AS INT) DESC",
+				cotKhoa, tenBang, cotKhoa, cotKhoa, prefix);
+
+		try (Connection con = KetNoiDatabase.getConnection(); PreparedStatement ps = con.prepareStatement(sql)) {
+
+			ps.setString(1, prefix + "%");
+			ResultSet rs = ps.executeQuery();
+
+			int so = 0;
+			if (rs.next()) {
+				String maMax = rs.getString(1); // VD: TTHD9
+				String soStr = maMax.replace(prefix, ""); // -> "9"
+				so = Integer.parseInt(soStr);
+			}
+
+			so++; // tăng lên 1
+			String maMoi = prefix + so; // VD: TTHD10
+			return maMoi;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	// ========== CHUYỂN CHUỖI TIỀN VỀ SỐ ==========
+	public double chuyenTienSangSo(String text) {
+		if (text == null || text.isEmpty())
+			return 0;
+		text = text.replaceAll("[^\\d.]", ""); // bỏ ký tự thừa
+		try {
+			return Double.parseDouble(text);
+		} catch (NumberFormatException e) {
+			return 0;
+		}
+	}
 
 }
